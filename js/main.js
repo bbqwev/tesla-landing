d9ff7efe6d88e4bb18dac9006230cd19c0cf8c2e
bbqwev <kassar12022001@gmail.com>
2022-09-13 11:39:03 +0300

    day5

---
+++ js/main.js
@@ -0,0 +1,20 @@
+function burgermenu(selector) {
+    let menu = $(selector);
+    let button = menu.find('.menu-btn');
+    let links = menu.find('.menu-list');
+    let overlay = menu.find('.menu-overlay');
+
+    button.on('click', (e) => {
+        e.preventDefault();
+        toggleMenu();        
+    });
+    links.on('click', () => toggleMenu());
+    overlay.on('click', () => toggleMenu());
+
+    function toggleMenu() {
+        menu.toggleClass('.menu_active');
+
+    }    
+}
+
+burgermenu('.menu');
--- main.js
+++
@@ -1,4 +0,0 @@
-$('.menu-btn').on('click', function(e) {
-    e.preventDefault();
-    $('.menu').toggleClass('menu_active');
-})
